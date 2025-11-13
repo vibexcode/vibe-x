ba91385befd718dcfbf1c690dfa55f4915e7652f
ugur <ugurkandemis@vibexprotocol.com>
2025-11-14 01:04:52 +0300

    Update exceptions.py

--- src/vibex/exceptions.py
+++ src/vibex/exceptions.py
@@ -1,1 +1,8 @@
+class MetaBlockEncodingError(Exception):
+    """Raised when an error occurs during MetaBlock encoding."""
+    pass
 
+
+class MetaBlockDecodingError(Exception):
+    """Raised when a marker or MetaBlock cannot be parsed correctly."""
+    pass
